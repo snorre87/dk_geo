3e8cc2d695bbd9d89f240000c4a32fa26a890580
Snorre Ralund <snorre87@users.noreply.github.com>
2021-12-18 14:26:03 +0100

    mapping script
    added mapping script

---
+++ geodk.py
@@ -0,0 +1,35 @@
+import pickle
+import requests
+with open('geo_lookups.pkl','wb') as f:
+  f.write(requests.get('https://github.com/snorre87/dk_geo/raw/main/geo_lookups.pkl').content)
+  f.close()
+pn2pnum,pnr2kom,p2kom,kom2reg,kom2reg,reg2reg = pickle.load(open('geo_lookups.pkl','rb'))
+
+def get_geo_info(geoname):
+  info = {}
+  for typ,d in zip(['Post_nummer','Kommune','Landsdel','Region'],[pn2pnum,(pnr2kom,p2kom),(kom2reg,kom2reg),reg2reg]): #
+    # check if input is either region, kommune, or postnr.
+    if type(d)==tuple:
+      for di in d:
+        if geoname in di:
+          info[typ] = di[geoname]
+          if typ=='Kommune':
+            info['Postnummer'] = geoname
+
+            geoname = info[typ]
+          if typ=='Post_nummer':
+            info['Navn'] = geoname
+          if typ=='Landsdel':
+            info['Kommune'] = geoname
+
+    else:
+      if geoname in d:
+        info[typ] = d
+        if typ=='Kommune':
+          info['Postnummer'] = geoname
+        if typ=='Post_nummer':
+            info['Navn'] = geoname
+        if typ=='Landsdel':
+            info['Kommune'] = geoname
+        geoname = info[typ]
+  return info
